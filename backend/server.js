7e29b4361bfddadefef03bddf5d4dbdd3c46d411
Goodygad <adebayogoodnesseri@gmail.com>
2026-07-17 09:52:14 +0100

    Prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,25 +3,36 @@ const notesRoutes = require("./src/routes/notesRoutes");
 const connectDB = require("./src/config/db.js");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+//const __dirname = path.resolve();
 
 connectDB();
 
 //allow requests from your frontend origin
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
-
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
 //middleware
 app.use(express.json()); //this middleware allows you to get access to req.body
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 //allow requests from your frontend origin
 
